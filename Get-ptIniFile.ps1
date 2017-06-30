340733dd5fbb971a354874d4b7d962036cd87466
maxshalkov <max.shalkov@gmail.com>
2017-06-30 15:51:06 +0300

    Start

---
+++ Get-ptIniFile.ps1
@@ -0,0 +1,34 @@
+﻿function Get-ptIniFile{
+
+    [cmdletbinding()]
+    Param(
+
+        [parameter(Mandatory=$true)]
+        [string]$FilePath
+
+    )
+
+    Begin{
+        $Ini = @{}
+    }
+    
+    Process{
+        Get-Content $FilePath | Where-Object {$_ -match "^[^;]"} | ForEach-Object {
+            switch ($_){
+                {$_ -match "\["} {
+                    $Head = $_ -replace "\[|\]" 
+                    $Ini[$Head] = @()
+                    return
+                }
+
+                {$Head -ne $null} {
+                    $Ini[$Head] += ,$_
+                }
+            }
+        }
+    }
+
+    End{
+        return $Ini
+    }
+}
